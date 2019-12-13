e05e46ee57fcfe07573112dbe01ec071c0257bd8
Stef Martens <stef.m@hotmail.be>
2019-12-13 15:58:03 +0100

    activitys aanmaken

--- app/src/main/java/com/example/mobilestefjan/LoginActivity.java
+++ app/src/main/java/com/example/mobilestefjan/LoginActivity.java
@@ -25,7 +25,7 @@ public class LoginActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.fragment_login);
+        setContentView(R.layout.activity_login);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
--- app/src/main/java/com/example/mobilestefjan/RegistreerActivity.java
+++ app/src/main/java/com/example/mobilestefjan/RegistreerActivity.java
@@ -24,7 +24,7 @@ public class RegistreerActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         requestWindowFeature(Window.FEATURE_NO_TITLE);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
-        setContentView(R.layout.fragment_registreer);
+        setContentView(R.layout.activity_registreren);
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
